1bc031433b5d63e1b8ddb3d118e729b3b885723d
Yuryi M <ymironchyk@gmail.com>
2019-05-24 07:40:44 -0700

    fixed stuff

--- src/Board.java
+++ src/Board.java
@@ -15,23 +15,25 @@ public class Board {
             //Goes through every row
             for(int r = 0; r < board.length; r++){
                 //Goes through every column in a row
-                for(int c = 0; c < board[r].length; r++){
+                for(int c = 0; c < board[r].length; c++){
                     int index = 0;
                     //if first letter of word matches character at coordinates
                     if(word.charAt(0) == board[r][c]){
                         //finds direction which word goes in
                         for(int i = 0; i < 9; i++){
                             try{
-                                if(board[(r-1) + i/3][(c-1) + i%3] == word.charAt(index)){
-
-                                    checkWord(word, i/3, i%3, r, c, index);
+                                //System.out.println(((r-1) + (i/3))+ ", " + ((c-1) + (i%3)) + "  " + board[((r-1) + (i/3))][((c-1) + (i%3))] + "  " + word.charAt(1));
+                                if(word.charAt(1) == board[((r-1) + (i/3))][((c-1) + (i%3))]){
+                                    checkDirection(word, );
                                 }
                             }
                             catch (ArrayIndexOutOfBoundsException ex){
-                                continue;
                             }
                         }
+
                     }
+
+
 
                 }
             }
@@ -39,7 +41,7 @@ public class Board {
 
     }
 
-    private void checkWord(String word, int xToAdd, int yToAdd, int x, int y, int index){
+    private void checkDirection(String word, int xToAdd, int yToAdd, int x, int y, int index){
         try{
             if((word.charAt(index) == board[(x-1) + xToAdd][(y-1) + yToAdd]) && (word.length()-1 != index)){
                 System.out.println((x-1) + xToAdd + ", " + (y-1) + yToAdd);
--- src/Main.java
+++ src/Main.java
@@ -3,17 +3,28 @@ import java.util.*;
 public class Main {
     public static void main(String[] args){
         Scanner keyboard = new Scanner(System.in);
-        char[][] board1 = {{'w','v','e','r','t','i','c','a','l','l'},
+        char[][] board1 = {{'x','v','e','r','t','i','c','a','l','l'},
                            {'r','o','o','a','f','f','l','s','a','b'},
                            {'a','c','r','i','l','i','a','t','o','a'},
-                           {'n','d','o','d','k','o','n','w','d','c'},
+                           {'n','d','o','d','k','o','n','x','d','c'},
                            {'d','r','k','e','s','o','o','d','d','k'},
                            {'o','e','e','p','z','e','g','l','i','w'},
                            {'m','s','i','i','h','o','a','e','r','a'},
                            {'a','l','r','k','r','r','i','r','e','r'},
                            {'k','o','d','i','d','e','d','r','c','d'},
                            {'h','e','l','w','s','l','e','u','t','h'}};
-        String[] words = {"week", "find", "random", "sleuth", "backward", "vertical", "diagonal", "wikipedia", "horizontal", "word search"};//keyboard.next().split(",");
+        /*
+        char[][] board1 = {{'w','v','e','r','t','i','c','a','l','l'},
+                {'r','o','o','a','f','f','l','s','a','b'},
+                {'a','c','r','i','l','i','a','t','o','a'},
+                {'n','d','o','d','k','o','n','w','d','c'},
+                {'d','r','k','e','s','o','o','d','d','k'},
+                {'o','e','e','p','z','e','g','l','i','w'},
+                {'m','s','i','i','h','o','a','e','r','a'},
+                {'a','l','r','k','r','r','i','r','e','r'},
+                {'k','o','d','i','d','e','d','r','c','d'},
+                {'h','e','l','w','s','l','e','u','t','h'}};*/
+        String[] words = {"wikipedia"};//, "find", "random", "sleuth", "backward", "vertical", "diagonal", "wikipedia", "horizontal", "word search"};//keyboard.next().split(",");
         Board testBoard1 = new Board(words, board1);
     }
 }
